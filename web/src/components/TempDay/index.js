61942df7173c196698b81ebcb84043bdbd61d9cf
Rafael Alves <alvesrafael17@hotmail.com>
2020-03-03 21:17:21 -0300

    criando modal para visualizar outros dias

--- web/src/components/TempDay/index.js
+++ web/src/components/TempDay/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './otherDay.css';
 import { FtoC, dataPadrao } from '../../assets/functions';
+import { Modal } from 'react-bootstrap';
 
-export default function TempDay({dia}) {
+export default function TempDay({dia}) {;
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
 
   return (
-    <div className="box">
+    <>
+    
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <button variant="secondary" onClick={handleClose}>
+          Close
+        </button>
+        <button variant="primary" onClick={handleClose}>
+          Save Changes
+        </button>
+      </Modal.Footer>
+    </Modal>
+
+    <div className="box" onClick={handleShow} >
 
       <div className="data">{dataPadrao(dia.Date)}</div>
 
@@ -23,6 +46,7 @@ export default function TempDay({dia}) {
 
       
     </div>
+    </>
   )
   
 }
