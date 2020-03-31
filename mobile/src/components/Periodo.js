a5e731785f1b4a7f21b95f2eceb0063059cb01d8
Rafael Alves <alvesrafael17@hotmail.com>
2020-03-31 17:45:45 -0300

    Periodo

---
+++ mobile/src/components/Periodo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function Periodo(dia){
+  return(
+    <View>
+      <Text>Canche de preciptação?<Text></Text></Text>
+      <Text>Intensidade<Text></Text></Text>
+      <Text>Tipo<Text></Text></Text>
+    </View>
+
+  )
+}
